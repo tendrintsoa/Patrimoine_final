985f4ddc74718c86154f789e172ed5645b51dfee
hervino <hei.hervino@gmail.com>
2024-09-06 16:02:53 +0300

    fix: The same as the last commit

---
+++ ui/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="project-title">Projet React</h1>
+    </div>
+  );
+};
+
+export default Home;
